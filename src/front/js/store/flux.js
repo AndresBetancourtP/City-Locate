774fc1eb7be1b857f41a89c2cd40b8d425bb1ef6
Delfin656 <juandelfin656@gmail.com>
2023-01-27 18:59:50 +0000

    Actualizacion del ultimo commit

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,77 +1,100 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-		  message: null,
-		  publicacion: [],
-		  token:
-		  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3MzY0ODcxMiwianRpIjoiNmI2ZWY0MjItODI1NS00Y2NkLTg3MTUtNDIyODI4ZmUwMTYyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InNhbXVlbGljIiwibmJmIjoxNjczNjQ4NzEyLCJleHAiOjE2NzM2NDk2MTJ9.9lXAA4AYSXT9fcdkMmia79kp1flkNfRKqGFhrFUlLK8",
-		  demo: [
-			{
-			  title: "FIRST",
-			  background: "white",
-			  initial: "white",
-			},
-			{
-			  title: "SECOND",
-			  background: "white",
-			  initial: "white",
-			},
-		  ],
-		},
-		actions: {
-		  // Use getActions to call a function within a fuction
-		  exampleFunction: () => {
-			getActions().changeColor(0, "green");
-		  },
+  return {
+    store: {
+      message: null,
+      publicacion: [],
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3MzY0ODcxMiwianRpIjoiNmI2ZWY0MjItODI1NS00Y2NkLTg3MTUtNDIyODI4ZmUwMTYyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InNhbXVlbGljIiwibmJmIjoxNjczNjQ4NzEyLCJleHAiOjE2NzM2NDk2MTJ9.9lXAA4AYSXT9fcdkMmia79kp1flkNfRKqGFhrFUlLK8",
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Usar getActions para llamar a una funcion dentro de una funcion
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-		  getMessage: async () => {
-			try {
-			  // fetching data from the backend
-			  const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+      getMessage: async () => {
+        try {
+          // Se obtienen datos del backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
 
-			  const data = await resp.json();
+          const data = await resp.json();
 
-			  setStore({ message: data.message });
-			  // don't forget to return something, that is how the async resolves
-			  return data;
-			} catch (error) {
-			  console.log("Error loading message from backend", error);
-			}
-		  },
-		  changeColor: (index, color) => {
-			//get the store
-			const store = getStore();
-	
-			//we have to loop the entire demo array to look for the respective index
-			//and change its color
-			const demo = store.demo.map((elm, i) => {
-			  if (i === index) elm.background = color;
-			  return elm;
-			});
-	
-			//reset the global store
-			setStore({ demo: demo });
-		  },
+          setStore({ message: data.message });
+          // Debes devolver algo para poder resolver el asincrono
+          return data;
+        } catch (error) {
+          console.log(
+            "Ocurrio un error al cargar mensaje desde el backend",
+            error
+          );
+        }
+      },
 
-		  postAnuncio: async (anuncio, url) => {
-			const store = getStore();
-	
-			let response = await fetch(process.env.BACKEND_URL + "/api/anuncios", {
-		@@ -80,6 +80,7 @@ const getState = ({ getStore, getActions, setStore }) => {
-			  },
-			  body: JSON.stringify({
-				content: chuit,
-				image: url,
-			  }), // body data type must match "Content-Type" header
-			});
-			let data = await response.json();
-			if (data) {
-			  alert("anuncio publicado satisfactoriamente");
-			  setStore({ anuncios: [data, ...store.anuncios] });
-			}
-		  },
-		},
-	  };
-	};
-	
-	export default getState;
+      getFeed: async () => {
+        try {
+          const resp = await fetch(process.env.BACKEND_URL + "/api/anuncios");
+          const data = await resp.json();
+
+          setStore({ publicacion: data });
+        } catch (err) {
+          console.log(err);
+        }
+      },
+
+      getProfile: async () => {
+        console.log("Vamos a repasar todo");
+      },
+
+      changeColor: (index, color) => {
+        //Obtienes desde el Store
+        const store = getStore();
+
+        //Hay que recorrer toda la matriz de demostracion para obtener el index
+        //Cambia su color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //Restablecer todo el Store
+        setStore({ demo: demo });
+      },
+
+      postTexto: async (texto, url) => {
+        const store = getStore();
+
+        let response = await fetch(process.env.BACKEND_URL + "/api/anuncios", {
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + store.token,
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: JSON.stringify({
+            content: texto,
+            image: url,
+          }), // body data type must match "Content-Type" header
+        });
+        let data = await response.json();
+        if (data) {
+          alert("Anuncio publicado satisfactoriamente");
+          setStore({ publicacion: [data, ...store.publicacion] });
+        }
+      },
+    },
+  };
+};
+
+export default getState;
